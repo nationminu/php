10f320f9a8ece88e4a16eaf3e2e4b7edb66238a3
nationminu <nationminu@users.noreply.github.com>
2019-09-04 11:40:22 +0900

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -2,12 +2,14 @@
 echo "WEBHOOK TEST : \n" ;  
 $hookfile=date("ymdhis").".json"
   
+$myfile = null; 
 $myfile = fopen("/opt/app-root/src/$hookfile", "w") or die("Unable to open file!");
 
 $txt = json_decode(json_encode($_POST));
 fwrite($myfile, $txt);
 $txt = json_decode(json_encode($_GET));
 fwrite($myfile, $txt);
+
 fclose($myfile); 
 
 echo "- Done\n" ;  
