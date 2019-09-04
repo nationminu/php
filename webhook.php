e29127db011156bf281c5a870afb8088df9ef164
nationminu <nationminu@users.noreply.github.com>
2019-09-04 13:33:31 +0900

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,12 +1,12 @@
 <?php
 echo "WEBHOOK TEST : \n" ;  
 
-$hookfile="test-".date('ymdhis').".json";
+$hookfile = $_SERVER['DOCUMENT_ROOT'] . "/test-".date('ymdhis').".json";
 
 $input = @file_get_contents("php://input");
 $event_json = json_encode(json_decode($input)); 
 
-file_put_contents("/opt/app-root/src/$hookfile", $event_json);
+file_put_contents($hookfile, $event_json);
 
 echo "- Done\n" ;  
 ?>
